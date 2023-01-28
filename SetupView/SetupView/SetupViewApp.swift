732857c14f9d46fd2dee21fdbf7318f9d6a30a66
pquimo <pquimo@users.noreply.github.com>
2023-01-28 10:54:17 -0800

    save one problematic version

--- SetupView/SetupView/SetupViewApp.swift
+++ SetupView/SetupView/SetupViewApp.swift
@@ -9,14 +9,51 @@ struct SetupViewApp: App {
   }
 }
 
-struct HomeView: View {
-  var body: some View {
-    Text("Hello, world!")
+struct Question {
+  let value1: Int
+  let value2: Int
+  let answer: Int
+
+  init(_ highestNumber: Int) {
+    value1 = Int.random(in: 1...highestNumber)
+    value2 = Int.random(in: 1...highestNumber)
+    answer = value1 * value2
+  }
+
+  var prompt: String {
+    "\(value1) x \(value2) = ?"
   }
 }
 
-struct QuestionView: View {
+struct HomeView1: View {
+  @State var highestNumber: Int? = nil
+
   var body: some View {
-    Text("The question goes here")
+    NavigationStack {
+      Picker("Highest Number", selection: $highestNumber) {
+        ForEach(4...12, id: \.self) { Text(String($0)) }
+      }
+      .pickerStyle(.wheel)
+      NavigationLink(destination: QuestionView(highestNumber: highestNumber!)) {
+        Text("Start")
+      }
+    }
+  }
+}
+
+struct QuestionView1: View {
+  @State var highestNumber: Int
+  @State var question: Question
+
+  init(highestNumber: Int) {
+    self.highestNumber = highestNumber
+    self.question = Question(highestNumber)
+  }
+
+  var body: some View {
+    Text(question.prompt)
+    Button("I got it") {
+      question = Question(highestNumber)
+    }
   }
 }
