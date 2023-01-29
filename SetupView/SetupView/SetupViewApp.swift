7db8bede1d36fc7271f02657e951d6e98f006994
eegrok <304507+eegrok@users.noreply.github.com>
2023-01-30 11:52:57 +1300

    Question version 2 modified, works without optionals...

--- SetupView/SetupView/SetupViewApp.swift
+++ SetupView/SetupView/SetupViewApp.swift
@@ -6,7 +6,7 @@ struct SetupViewApp: App {
     WindowGroup {
       Text("uncomment a HomeView to see each of the 4 bad examples")
       // HomeView1()
-      // HomeView2()
+      HomeView2()
       // HomeView3()
       // HomeView4()
     }
@@ -63,7 +63,7 @@ struct QuestionView1: View {
 }
 
 struct HomeView2: View {
-  @State var highestNumber: Int? = nil
+  @State var highestNumber: Int = 12
 
   var body: some View {
     NavigationStack {
@@ -79,19 +79,22 @@ struct HomeView2: View {
 }
 
 struct QuestionView2: View {
-  @Binding var highestNumber: Int?
-  @State var question: Question? = nil
+  @Binding var highestNumber: Int
+  @State var question: Question
 
+  init(highestNumber: Binding<Int>) {
+    self._highestNumber = highestNumber
+    question = Question(highestNumber.wrappedValue)
+  }
+  
   var body: some View {
-    if let questionR = question {
-      Text(questionR.prompt)
+      Text(question.prompt)
       Button("I got it") {
-        question = Question(highestNumber!)
+        question = Question(highestNumber)
       }.onChange(of: highestNumber) { _ in
-        question = Question(highestNumber!)
+        question = Question(highestNumber)
       }
     }
-  }
 }
 
 struct HomeView3: View {
